45ac49f7ca56db064137ced733bf24fe6025ed7c
ngunjiri <owen2ngunjiri@gmail.com>
2023-09-14 23:17:02 +0300

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
 import localFont from "next/font/local";
 import type { AppProps } from "next/app";
-const proximaFont = localFont({ src: "Proxima.otf" });
+const proximaFont = localFont({ src: "Proxima.otf", variable: '--Proxima-Nova-Rg' });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={proximaFont.className}>
+    <main className={`${proximaFont.variable} font-sans`}>
       <Component {...pageProps} />
     </main>
   );
